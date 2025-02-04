f535c6cba39127cb036aae83375853889eadd13a
aman1233455 <Aman.br.edge@gmail.com>
2025-02-04 09:15:59 +0530

    Create tflite.py

---
+++ tflite.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+
+# Load your trained model
+model = tf.keras.models.load_model('your_model.h5')
+
+# Convert the model to TensorFlow Lite format
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+tflite_model = converter.convert()
+
+# Save the model
+with open('model.tflite', 'wb') as f:
+    f.write(tflite_model)
